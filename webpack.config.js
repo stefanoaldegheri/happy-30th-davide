daf43d5f3666ed0d238a64e0f1a03c240170ace8
saldegheri <saldegheri@axist.it>
2025-09-27 03:07:23 +0200

    packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === 'production';
@@ -36,6 +37,11 @@ module.exports = (env, argv) => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './public/index.html',
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: 'public/images', to: 'images' },
+        ],
       }),
     ],
     devServer: {
